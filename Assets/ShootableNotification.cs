e419e0c765a47ae1c2900b12a533e928533f72f1
iwannasee <ueto.kiei@gmail.com>
2019-01-25 17:53:31 +0900

    #Added img for effect in advance

---
+++ Assets/ShootableNotification.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootableNotification : MonoBehaviour {
+	float maxScale = 1.3f;
+	float minScale = 0.9f;
+	bool isGrowing = true;
+	// Update is called once per frame
+	void Update () {
+		if(isGrowing){
+			transform.localScale = new Vector2(
+				transform.localScale.x + Time.deltaTime, 
+				transform.localScale.y + Time.deltaTime
+			);
+			if(transform.localScale.x >= maxScale){
+				isGrowing = false;
+			}
+		}else{
+			transform.localScale = new Vector2(
+				transform.localScale.x - Time.deltaTime, 
+				transform.localScale.y - Time.deltaTime
+			);
+
+			if(transform.localScale.x <= minScale){
+				isGrowing = true;
+			}
+		}
+	}
+}
